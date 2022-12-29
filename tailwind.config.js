ba6ac12c7dfae6d84b3d2b3358983a6bb28a38f8
Michal <michalstolarski@gmail.com>
2022-12-29 14:33:56 +0100

    feat: extend tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: [...fontFamily.sans],
-      serif: ["var(--font-open-sans)", ...fontFamily.serif],
+      sans: [...defaultTheme.fontFamily.sans],
+      serif: ["var(--font-open-sans)", ...defaultTheme.fontFamily.serif],
       cursive: ["var(--font-bebas-neue)", "cursive"],
+    },
+    fontSize: {
+      ...defaultTheme.fontSize,
+      timer: [
+        "2.5rem",
+        {
+          lineHeight: "1",
+          letterSpacing: "0",
+        },
+      ],
+      title: [
+        "5rem",
+        {
+          lineHeight: "0.875em",
+          letterSpacing: "0.06em",
+        },
+      ],
     },
   },
   plugins: [],
